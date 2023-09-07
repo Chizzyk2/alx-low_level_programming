dc0ba3cd695e564fedfa7ba3f56b2bcb00558739
Chizzyk2 <ziterenna@gmail.com>
2023-09-07 07:14:33 -0700

    Hello, world!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - C program
- *
- * Return: 1
+ * 
+ * Return: 1 (Passed)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
