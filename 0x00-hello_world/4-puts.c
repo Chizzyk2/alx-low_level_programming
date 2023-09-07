0a67ee3ba943ab41616657581311d28130cfaf15
Chizzyk2 <ziterenna@gmail.com>
2023-09-07 04:26:01 -0700

    Hello-World!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main - C program that displays a sentence using puts command
+ * Return: Is 0  (Passed)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
