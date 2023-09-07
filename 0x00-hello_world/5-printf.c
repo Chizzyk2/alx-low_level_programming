de58f97f4bf453aa2d5f02f4353905acd147df9e
Chizzyk2 <ziterenna@gmail.com>
2023-09-07 04:54:01 -0700

    Hello-World!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that displays a sentence using printf command
+ *
+ * Return: Is 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
