81a2987cedd45b2163f19c7f20ed4fa64a3e0686
Chizzyk2 <ziterenna@gmail.com>
2023-09-12 05:24:20 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Alphabet
+ *
+ * Return: Is 0
+ */
+void print_alphabet(void)
+{
+	char albet;
+	for (albet = 'a'; albet <= 'z'; albet++)
+		_putchar(albet);
+	_putchar('\n');
+}
