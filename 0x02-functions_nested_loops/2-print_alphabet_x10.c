82166e966e52d69eadcb8b0b00578974f6adc6a9
Chizzyk2 <ziterenna@gmail.com>
2023-09-12 05:39:01 -0700

    functions and loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Alphabet
+ *
+ * Return: Is void
+ */
+
+void print_alphabet_x10(void)
+{
+char albet;
+int i;
+i = 0;
+while (i < 10)
+{
+for (albet = 'a'; ch <= 'z'; albet++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
+
+}
+
