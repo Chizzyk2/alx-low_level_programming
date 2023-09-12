e419d36adb392adbb004b41e8ec759b3ecd34435
Chizzyk2 <ziterenna@gmail.com>
2023-09-12 06:00:01 -0700

    Functions and loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value of an integer
+ * @n : number to check
+ * Return:Is 0 or 1
+ */
+
+int _abs(int n)
+{
+
+return (n >= 0 ? n : n * -1);
+
+}
