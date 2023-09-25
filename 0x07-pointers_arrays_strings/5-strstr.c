82cf70828907c427e44dcdd441d5ca53ef08fef5
Chizzyk2 <ziterenna@gmail.com>
2023-09-25 00:58:43 -0700

    C more pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _strstr - It will locate a function 
+* @haystack: It will point to a char
+* @needle: It points to a char
+* Return: Is 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
