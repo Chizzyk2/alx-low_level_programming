727c5644faa6c9c2962ee8a34a971c7895d5af82
Chizzyk2 <ziterenna@gmail.com>
2023-09-25 05:18:54 -0700

    C pointers

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
- * set_string - converts a pointer val to char
- * @s: It points
- * @to: Is char
+ * set_string - It points to a char
+ * @s: It points to a string
+ * @to: set s to
+ * Return:Null
  */
 void set_string(char **s, char *to)
 {
 	*s = to;
-
+}
