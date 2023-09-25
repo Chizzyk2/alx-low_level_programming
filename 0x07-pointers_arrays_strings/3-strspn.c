77f4415dc8b68fcd264640326462c7367d853a78
Chizzyk2 <ziterenna@gmail.com>
2023-09-25 00:50:24 -0700

    C more pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strspn - It will Get the length of substring
+* @s: substring is stored
+* @accept: Substring chars is accepted
+* Return: Is C
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	char *t = accept;
+
+	while (*s++)
+	{
+		while (*accept++)
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
+	return (c);
+}
