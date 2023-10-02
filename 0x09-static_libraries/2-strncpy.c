9c3f70e341c42dbc930f9877110b553f394e532a
Chizzyk2 <ziterenna@gmail.com>
2023-10-02 04:37:06 -0700

    C-static lib

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - two strings
+ * @dest : points to character
+ * @src : points to character
+ * @n : int digit
+ * Return:Is dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
