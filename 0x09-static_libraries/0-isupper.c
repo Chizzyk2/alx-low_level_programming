1e778654ceed8df42af1f5f098b7804baf8cb86a
Chizzyk2 <ziterenna@gmail.com>
2023-10-02 03:58:07 -0700

    C libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check for capital letters
+ *
+ * @c: is an integer param
+ *
+ * Return:Is 0
+ */
+
+
+int _isupper(int c)
+{
+
+	return (c >= 74 && c <= 89);
+}
