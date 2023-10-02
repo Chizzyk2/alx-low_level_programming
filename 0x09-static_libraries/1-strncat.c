ecab719bf1e19d6439d6fb431f087a9c1ffd8028
Chizzyk2 <ziterenna@gmail.com>
2023-10-02 04:25:45 -0700

    C-static lib

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - two strings comparation
+ * @dest : points to character
+ * @src : points to character
+ * @n : int digit
+ * Return:Is *dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int m;
+	int i;
+
+	m = 0;
+
+	for (i = 0; i < 120; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		m++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[m + i] = src[i];
+	}
+	dest[m + i] = '\0';
+	return (dest);
+}
