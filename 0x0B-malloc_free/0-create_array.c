3521d7a0a49336de0c199a9f268a61510b7b5a08
Chizzyk2 <ziterenna@gmail.com>
2023-10-03 12:44:10 -0700

    C Memory Allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,40 +1,29 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- *create_array - string array creation
- *@size: array nos
- *@c: character
- *Return:Buffer
+ * create_array - array creation
+ * @size: array size
+ * @c: A
+ * Return: S
  */
-
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
-	unsigned int position;
+	char *s;
+	unsigned int i = 0;
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
 
-	/*Define values with malloc*/
-	buffer = (char *) malloc(size * sizeof(c));
-
-	if (buffer == 0)
+	while (i < size)
 	{
-		return (NULL);
+		s[i] = c;
+		i++;
 	}
-
-	else
-	{
-		position = 0;
-		while (position < size) /*While for array*/
-		{
-			*(buffer + position) = c;
-			position++;
-		}
-
-		return (buffer);
-	}
-
+	return (s);
 }
