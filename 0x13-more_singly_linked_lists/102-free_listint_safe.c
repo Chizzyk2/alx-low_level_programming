5a3e21f1be5da62b0885d4f7c3790cbae2581b87
Chizzyk2 <ziterenna@gmail.com>
2023-10-23 23:27:52 -0700

    Singly linked

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,38 +1,45 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
 /**
- * free_listint_safe - frees list
+ * free_listint_safe - free list
  * @h: points
- *
  * Return: freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
+	size_t counter = 0;
 	listint_t *temp;
 
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
+	temp = *h;
+	while (temp)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			*h = NULL;
-			len++;
-			break;
-		}
+		temp = *h;
+		temp = temp->next;
+		free_list(temp);
+		counter++;
 	}
-
 	*h = NULL;
 
-	return (len);
+	return (counter);
+}
+
+/**
+ * free_list - free list
+ * @head: points
+ * Return: Null
+ */
+void free_list(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head)
+	{
+		temp = head;
+		temp = temp->next;
+		free(temp);
+		free_list(temp);
+	}
+	free(head);
 }
